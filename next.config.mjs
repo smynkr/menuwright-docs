ff3ad7a9e570af2955c21afd6c5d9e01cc6972f5
VibeReen <amreen.s.rahman@gmail.com>
2026-08-14 04:50:18 -0700

    chore: add fail-closed Sentry monitoring

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { withSentryConfig } from '@sentry/nextjs';
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
@@ -38,4 +39,4 @@ const config = {
   },
 };
 
-export default withMDX(config);
+export default withSentryConfig(withMDX(config), { silent: true });
